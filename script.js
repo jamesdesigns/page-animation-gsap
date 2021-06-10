6406e667cbee18326b52a447b0de2584e77ef632
James Hooper <jameshooperdesigns@gmail.com>
2021-06-10 15:52:16 -0600

    Edit content and fix styles to work

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const logo = document.querySelector('#logo');
 const hamburger = document.querySelector('.hamburger');
 const headline = document.querySelector('.headline');
 
+
 const tl = new TimelineMax();
 
 tl.fromTo(hero,1, {height: "0%"}, {height: "80%", ease: Power2.easeInOut })
